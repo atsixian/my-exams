b158c1274fff1c34bf776127ce2710d1cce48e3a
Jiahao <jahowchen@outlook.com>
2020-02-12 14:38:52 -0500

    Update filterExams
    Don't touch this file until I fix it

--- src/Functions/filterExams.ts
+++ src/Functions/filterExams.ts
@@ -1,10 +1,25 @@
 //find exams in data.json from current courses
 import exams from '../Assets/data.json'
 //const exams = require('../../assets/data.json');
+type examInfo = {
+    "COURSE NUMBER": string,
+    "SECTION": string,
+    "TITLE": string,
+    "EXAM DATE":string,
+    "TIME": string,
+    "BUILDING": string | null,
+    "ROOM": string | null ,
+    "ROW": string | null,
+    "FROM": string | null,
+    "TO": string | null,
+};
+const data : Array<examInfo> = exams["data"];
+type courseInfo = {'COURSE NUMBER':string,'SECTION':string};
+type filterType = (courses : Array<courseInfo>) => Array<examInfo>;
 
-export const filterExams = (courses : object[]) =>
-    exams.filter((exam : object) => 
-        courses.some((course : object) =>
+export const filterExams : filterType = (courses) =>
+    exams.filter((exam : examInfo) : => 
+        courses.some((course : courseInfo) =>
         course["COURSE NUMBER"]===exam["COURSE NUMBER"] && course["SECTION"]===exam["SECTION"])
     );
 ;
