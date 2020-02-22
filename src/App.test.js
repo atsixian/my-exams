5dab309ff813e4ec24ad2319a5846bda3dbf6229
Jiahao <jahowchen@outlook.com>
2020-02-22 15:07:13 -0500

    Add test for filterCourses

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,107 @@
 import toTable from './Functions/toTable';
 import filterExams from './Functions/filterExams.ts'
+import {filterCourses} from './Functions/filterCourses';
 
+let list0 = [
+  {
+    "name": "COMP-202-002: Foundations of Programming",
+    "semester": "Fall 2018"
+  },
+  {
+    "name": "GSFS-250-001: Sexual and Gender Diversity St",
+    "semester": "Fall 2018"
+  },
+  {
+    "name": "MATH-141-002: Calculus 2",
+    "semester": "Fall 2018"
+  },
+  {
+    "name": "MATH-133-001: Linear Algebra and Geometry",
+    "semester": "Fall 2018"
+  },
+  {
+    "name": "PHYS-101-001: Intro Physics - Mechanics",
+    "semester": "Fall 2018"
+  },
+  {
+    "name": "COMP-250-002: Intro to Computer Science",
+    "semester": "Winter 2019"
+  },
+  {
+    "name": "COMP-206-001: Intro to Software Systems",
+    "semester": "Winter 2019"
+  },
+  {
+    "name": "LING-201-001: Introduction to Linguistics",
+    "semester": "Winter 2019"
+  },
+  {
+    "name": "PHYS-183-001: The Milky Way Inside and Out",
+    "semester": "Winter 2019"
+  },
+  {
+    "name": "MATH-133-016: Linear Algebra and Geometry",
+    "semester": "Fall 2018"
+  },
+  {
+    "name": "MATH-141-003: Calculus 2",
+    "semester": "Fall 2018"
+  },
+  {
+    "name": "GSFS-250-004: Sexual and Gender Diversity St",
+    "semester": "Fall 2018"
+  },
+  {
+    "name": "PHYS-101-994: Intro Physics - Mechanics",
+    "semester": "Fall 2018"
+  },
+  {
+    "name": "LING-201-003: Introduction to Linguistics",
+    "semester": "Winter 2019"
+  },
+  {
+    "name": "LING-330-001: Phonetics",
+    "semester": "Fall 2019"
+  },
+  {
+    "name": "MATH-240-001: Discrete Structures",
+    "semester": "Fall 2019"
+  },
+  {
+    "name": "COMP-302-002: Programming Lang & Paradigms",
+    "semester": "Fall 2019"
+  },
+  {
+    "name": "COMP-230-001: Logic and Computability",
+    "semester": "Fall 2019"
+  },
+  {
+    "name": "EXTL-TASC-001: Term Away-Science",
+    "semester": "Summer 2019"
+  },
+  {
+    "name": "MATH-240-001: Discrete Structure",
+    "semester": "Winter 2020"
+  },
+  {
+    "name": "COMP-250-001: Intro to CS",
+    "semester": "Winter 2020"
+  },
+  {
+    "name": "LING-330-005: Phonetics",
+    "semester": "Fall 2019"
+  }
+];
 
 const courses0 = 
 [
   {"COURSE NUMBER": "COMP 250", "SECTION": "001"},
   {"COURSE NUMBER": "MATH 240", "SECTION": "001"},
 ];
+
+test('testing filterCourses', () => {
+  expect(filterCourses(list0).toEqual(courses0));
+});
 
 const exams0 = [
   {
